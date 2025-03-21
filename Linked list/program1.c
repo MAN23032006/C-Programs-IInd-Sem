d837ff147a66b1d19c2db9a4346e792cbd842896
Manik Sharma <2024a6r002@mietjammu.in>
2025-03-21 11:14:46 +0530

    Add files via upload

---
+++ Linked list/program1.c
@@ -0,0 +1,15 @@
+/// SIMPLE LINKED LIST
+
+
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node{
+    int data;
+    struct node *next;
+} Node;
+int main(){
+    while(1){
+        printf("Simple Linked list !!!!!\n");
+        printf("I")
+    }
+}
